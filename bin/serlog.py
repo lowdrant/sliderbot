61e4240634cc4e58225453fd0ce523000b8e96f0
lowdrant <lmanderson42@gmail.com>
2023-05-01 22:35:01 -0400

    serlog.py code formatting

--- bin/serlog.py
+++ bin/serlog.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
-from serial import Serial,SerialException
-from time import time,sleep
+from time import sleep, time
 
+from serial import Serial, SerialException
 
 parser = ArgumentParser('Log serial data from serial')
-parser.add_argument('fn',help='log filename')
-parser.add_argument('--tty', default=None,help='serial port filename')
-parser.add_argument('--tf', default=10,type=float,help='logger runtime (seconds)')
-parser.add_argument('--baud',type=int,default=1000000,help='baudrate')
+parser.add_argument('fn', help='log filename')
+parser.add_argument('--tty', default=None, help='serial port filename')
+parser.add_argument('--tf', default=10, type=float,
+                    help='logger runtime (seconds)')
+parser.add_argument('--baud', type=int, default=1000000, help='baudrate')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # Arg Parse
-    args=parser.parse_args()
+    args = parser.parse_args()
     tf = args.tf
     if args.tty is not None:
         ser = Serial(args.tty, args.baud)
     else:
         for i in range(4):
             try:
-                ser = Serial(f'/dev/ttyACM{i}',args.baud)
+                ser = Serial(f'/dev/ttyACM{i}', args.baud)
             except SerialException:
                 pass
             else:
@@ -31,8 +32,9 @@ if __name__=='__main__':
     ser.read(ser.in_waiting)
 
     # Read and Dump
-    print(f'Reading from {ser.port} at {ser.baudrate} baud for {tf} seconds to {args.fn}')
-    f = open(args.fn,'w')
+    print(
+        f'Reading from {ser.port} at {ser.baudrate} baud for {tf} seconds to {args.fn}')
+    f = open(args.fn, 'w')
     ser.read(ser.in_waiting)
     tr = time()
     try:
